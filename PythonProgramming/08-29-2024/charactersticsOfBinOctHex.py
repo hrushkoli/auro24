7d3b39d368db407103dcd9902e6428e20de91558
hrushkoli <hrushkoli1@gmail.com>
2024-08-29 13:35:52 +0530

    08-29-2024

---
+++ PythonProgramming/08-29-2024/charactersticsOfBinOctHex.py
@@ -0,0 +1,7 @@
+# Note: Despite being defined as a binary/octal/hex number, the number is still stored as a int
+a = 0o657
+print(type(a))
+print(type(0o657))
+print(a)
+print(0o657)
+print(oct(0o657))
--- PythonProgramming/08-29-2024/typeConversion.py
+++ PythonProgramming/08-29-2024/typeConversion.py
@@ -1,8 +1,27 @@
 # Example of Type Conversion
 
 # There are 4 types of Data 
+#  -- Binary
 #  -- Decimal
 #  -- Octal
 #  -- Hexa
 
+x = 123
+y = bin(x)
+print(y)
+print(0b1111011) ## Print converts the binary value to decimal and prints it
 
+a = 0o657
+print(type(a))
+print(type(0o657))
+print(a) ## Print converts the octal number to decimal and prints it
+
+b = 0x657abc
+print(b) ## Print converts the hexa number to decimal and prints it
+
+c = oct(x)
+print(c)
+
+d = 0b101110110
+z = oct(d)
+print(z)
