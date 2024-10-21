7e3d527f8fce64bed3177eb5134322cc8572ca44
hrushkoli <hrushkoli1@gmail.com>
2024-10-21 14:15:34 +0530

    10-21-2024 Python

--- PythonProgramming/10-21-2024/studentSql.py
+++ PythonProgramming/10-21-2024/studentSql.py
@@ -45,4 +45,36 @@ def insert_student_dta(mydb,cursor):
         print("Error Inserting Data: ",err)
 
 def read_student_data(mydb,cursor):
-    
+    query = "SELECT * FROM student"
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+    except mysql.connector.Error as err:
+        print("Error Reading Data: ",err)
+
+def update_student_data(mydb,cursor):
+    student_id = input("Enter Student ID to Update: ")
+    set_clause = input("Enter SET Clause (e.g.,(name = John, age = 20) :")
+    query = f"UPDATE student SET {set_clause} WHERE id = {student_id}"
+
+    try:
+        cursor.execute(query)
+        mydb.commit()
+        print("Student Data Updated Successfully")
+
+    except mysql.connector.Error as err:
+        print("Error Updating Data: ",err)
+
+def delete_student_data(mydb,cursor):
+    student_id = input("Enter Student ID to Update: ")
+    query= f"DELETE FROM student WHERE id = {student_id}"
+
+    try:
+        cursor.execute(query)
+        mydb.commit()
+        print("Student Data Updated Successfully")
+    except mysql.connector.Error as err:
+        print("Error Updating Data: ",err)
+
